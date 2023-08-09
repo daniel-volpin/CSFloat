a957b2c962bea670f0ee2dc38486e5d9f8013e97
Daniel Volpin <d.volpin@student.vu.nl>
2023-08-09 15:59:27 +0200

    refactored csfloat_page.dart

--- lib/csfloat_page.dart
+++ lib/csfloat_page.dart
@@ -7,7 +7,56 @@ import 'csfloat_trade_data.dart';
 class PostsPage extends StatelessWidget {
   final HttpService httpService = HttpService();
 
-  PostsPage({super.key});
+  PostsPage({Key? key}) : super(key: key);
+
+  Widget buildCsfloatCard(CSFloat csfloatData) {
+    return Card(
+      elevation: 2,
+      child: ListTile(
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Listing ID: ${csfloatData.itemID}"),
+            const SizedBox(height: 4),
+            Text("Item price: ${csfloatData.price}"),
+            const SizedBox(height: 4),
+            Text("Item Information: ${csfloatData.itemInfo.marketHashName}"),
+            if (csfloatData.sellerInfo!.steamID != null) ...[
+              const SizedBox(height: 4),
+              Text("Seller Steam ID: ${csfloatData.sellerInfo!.steamID}"),
+            ],
+            if (csfloatData.itemInfo.inspectLink != null &&
+                csfloatData.itemInfo.inspectLink is String &&
+                csfloatData.itemInfo.inspectLink.isNotEmpty) ...[
+              const SizedBox(height: 4),
+              buildInspectionLink(csfloatData.itemInfo.inspectLink),
+            ],
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildInspectionLink(String inspectLink) {
+    return RichText(
+      text: TextSpan(
+        children: [
+          const TextSpan(text: "Inspection URL: "),
+          TextSpan(
+            text: inspectLink,
+            style: const TextStyle(
+              color: Colors.blue,
+              decoration: TextDecoration.underline,
+            ),
+            recognizer: TapGestureRecognizer()
+              ..onTap = () {
+                launchUrl(Uri.parse(inspectLink));
+              },
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +64,7 @@ class PostsPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("CSFloat Listed Items"),
       ),
-      body: FutureBuilder(
+      body: FutureBuilder<List<CSFloat>>(
         future: httpService.getPosts(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
@@ -25,51 +74,12 @@ class PostsPage extends StatelessWidget {
           } else if (snapshot.hasData) {
             final csfloatData = snapshot.data!;
             return ListView(
-              children: csfloatData.map<Widget>(
-                (CSFloat csfloatData) {
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                    child: Card(
-                      elevation: 2,
-                      child: ListTile(
-                        title: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text("Listing ID: ${csfloatData.itemID}"),
-                            const SizedBox(height: 4),
-                            Text("Item price: ${csfloatData.price}"),
-                            const SizedBox(height: 4),
-                            Text("Item Information: ${csfloatData.itemInfo.marketHashName}"),
-                            if (csfloatData.sellerInfo!.steamID != null)
-                              const SizedBox(height: 4),
-                              Text("Seller Steam ID: ${csfloatData.sellerInfo!.steamID}"),
-                            if (csfloatData.itemInfo.inspectLink != null && csfloatData.itemInfo.inspectLink is String && csfloatData.itemInfo.inspectLink.isNotEmpty)
-                              const SizedBox(height: 4),
-                              RichText(
-                                text: TextSpan(
-                                  children: [
-                                    const TextSpan(text: "Inspection URL: "),
-                                    TextSpan(
-                                      text: csfloatData.itemInfo.inspectLink,
-                                      style: const TextStyle(
-                                        color: Colors.blue,
-                                        decoration: TextDecoration.underline,
-                                      ),
-                                      recognizer: TapGestureRecognizer()
-                                        ..onTap = () {
-                                          launchUrl(Uri.parse(csfloatData.itemInfo.inspectLink));
-                                        },
-                                    ),
-                                  ],
-                                ),
-                              ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ).toList(),
+              children: csfloatData.map<Widget>((CSFloat csfloat) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                  child: buildCsfloatCard(csfloat),
+                );
+              }).toList(),
             );
           } else {
             return const Center(child: CircularProgressIndicator());
